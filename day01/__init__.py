743117279d262c33870816435da6247f23724451
ch42 <ch42@email.com>
2022-07-22 21:26:06 +0800

    提交hello

---
+++ day01/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/7/22 21:24
+Copyright (c) 2022/7/22, 海牛学院版权所有.
+@author: chenhe
+"""
---
+++ day01/hello_world.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+hello_world.py
+Created on 2022/7/22 21:24
+Copyright (c) 2022/7/22, 海牛学院版权所有.
+@author: chenhe
+"""
+print('hello')
